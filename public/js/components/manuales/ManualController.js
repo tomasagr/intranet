4bbf5f49f5bc727b2e976c431f4151ab9ff8e109
Lucas Michailian <lucasmichailian@gmail.com>
2017-05-21 13:05:55 -0300

    data

---
+++ public/js/components/manuales/ManualController.js
@@ -0,0 +1,29 @@
+angular.module('app.manual-controller', [])
+.controller('ManualController', ['$scope', '$http', '$sce', 'SectorsService',  function($scope, $http, $sce, SectorsService) {
+  $scope.search = {}
+  $scope.link = ""
+  $http.get('/api/manuals')
+    .then(function(response) {
+      $scope.manual = response.data
+    })
+    .catch(function(error) {
+      console.error(error)
+    })
+
+  $scope.modalData = function(link) {
+    $scope.linkData = $sce.trustAsResourceUrl('https://www.youtube.com/embed/'+link)
+  }
+
+  SectorsService.getAll()
+    .then(function(response) {
+      $scope.sectors = response
+    })
+
+  $scope.filterSector = function(id) {
+    $scope.search.sector_id = id || ''
+  }
+
+  $scope.closeModal = function() {
+    $scope.linkData = ""
+  }
+}])
