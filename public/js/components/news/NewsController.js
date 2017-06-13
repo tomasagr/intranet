aa78e3f53e75b9389ff2e8b042a67a53ef2bdd2a
Lucas Michailian <lucasmichailian@gmail.com>
2017-06-13 17:57:39 -0300

    fix

--- public/js/components/news/NewsController.js
+++ public/js/components/news/NewsController.js
@@ -1,34 +1,34 @@
 angular.module('app.news-controlller', [])
-  .controller('NewsController', ['$scope', '$http', function ($scope, $http) {
-    $scope.background = '#ccc'
+    .controller('NewsController', ['$scope', '$http', function ($scope, $http) {
+      $scope.background = '#ccc'
 
-    var pathname = window.location.pathname
-    var url = '/api/news?limit=4'
+      var pathname = window.location.pathname
+      var url = '/api/news'
 
-    if (pathname == '/institutional') {
-      url = '/api/news?limit=4&category=2'
-    } else if (pathname == '/informal') {
-      url = '/api/news?limit=4&category=1'
-    }
+      if (pathname == '/institutional') {
+          url = '/api/news?limit=4&category=2'
+        } else if (pathname == '/informal') {
+          url = '/api/news?limit=4&category=1'
+        }
 
-    $http.get(url)
-      .then(function (response) {
-        $scope.lastNews = response.data
-        $scope.selected = response.data[0]
+      $http.get(url)
+            .then(function (response) {
+              $scope.lastNews = response.data.slice(0, 4)
+              $scope.selected = response.data[0]
 
-        $scope.institucionales = $scope.lastNews.filter(function (element) {
-          return element.category_id === 2
-        })
+              $scope.institucionales = response.data.filter(function (element) {
+                  return element.category_id === 2
+                }).slice(0, 3)
 
-        $scope.informales = $scope.lastNews.filter(function (element) {
-          return element.category_id === 1
-        })
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
+              $scope.informales = response.data.filter(function (element) {
+                  return element.category_id === 1
+                }).slice(0, 3)
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
 
-    $scope.changeSelected = function (index) {
-      $scope.selected = $scope.lastNews[index]
-    }
-  }])
+      $scope.changeSelected = function (index) {
+          $scope.selected = $scope.lastNews[index]
+        }
+    }])
